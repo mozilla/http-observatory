d9b96451549894bd06a7e54430efb4c17b9a1512
April King <april@mozilla.com>
2016-02-04 12:04:53 -0600

    earning @chuckharmston style points!

--- database/__init__.py
+++ database/__init__.py
@@ -1,6 +1,6 @@
-from .database import get_cursor,\
-    insert_scan, insert_scan_grade, insert_test_result,\
-    select_scan_recent_scan, select_site_id, select_test_results
+from .database import (get_cursor,
+                      insert_scan, insert_scan_grade, insert_test_result,
+                      select_scan_recent_scan, select_site_id, select_test_results)
 
 __all__ = [
     'get_cursor',
