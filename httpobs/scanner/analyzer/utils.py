3dbe8422d58aadc78f3d6daacb5c056984ad5adc
April King <april@mozilla.com>
2016-03-02 14:01:38 -0600

    Bunch of updates to header tests
    - Pull in Google HSTS preload list on startup
    - Add support for HSTS preload list in HSTS test
    - Modify CSP test to include data as parsed header, instead of raw
    - Rewrite CSP test testing
    - Modify XFO test to allow for frame-ancestors in CSP
    - Rewrite X-XSS-Protection test
    - Allow sites to not set X-XSS-Protection if strong CSP policy exists

---
+++ httpobs/scanner/analyzer/utils.py
@@ -0,0 +1,50 @@
+import requests
+import sys
+
+from base64 import b64decode
+from json import loads
+
+
+HSTS_URL = ('https://chromium.googlesource.com/chromium'
+            '/src/net/+/master/http/transport_security_state_static.json?format=TEXT')
+hsts = {}
+
+# Download the Google HSTS Preload List
+try:
+    print('Retrieving the Google HSTS Preload list', file=sys.stderr)
+    r = b64decode(requests.get(HSTS_URL).text).decode('utf-8').split('\n')
+
+    # Remove all the comments
+    r = ''.join([line.split('// ')[0] for line in r if line.strip() != '//'])
+
+    r = loads(r)
+
+    # Mapping of site -> whether it includes subdomains
+    hsts = {site['name']: site['include_subdomains'] for site in r['entries'] if 'include_subdomains' in site}
+
+except:
+    print('Unable to download the Google HSTS Preload list; exiting', file=sys.stderr)
+    exit(1)
+
+
+def is_hsts_preloaded(hostname):
+    # Just return true if the hostname is the HSTS list -- no need to see if includeSubDomains is set or not
+    if hostname in hsts:
+        return True
+
+    # Either the hostname is in the list *or* the TLD is and includeSubDomains is true
+    host = hostname.split('.')
+    levels = len(host)
+
+    # If hostname is foo.bar.baz.mozilla.org, check bar.baz.mozilla.org, baz.mozilla.org, mozilla.org, and .org
+    for i in range(1, levels):
+        if hsts.get('.'.join(host[i:levels])):
+            return True
+
+    return False
+
+
+# Let this file be run directly so you can see the JSON for the Google HSTS thingie
+if __name__ == '__main__':
+    for key in hsts.keys():
+        print(key)
