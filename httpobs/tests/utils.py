4e0090937b18f3ad1face7ce231ff09542716b8b
April King <april@mozilla.com>
2016-03-07 15:50:27 -0600

    Add tests for CORS, Redirection

--- httpobs/tests/utils.py
+++ httpobs/tests/utils.py
@@ -25,11 +25,15 @@ def empty_requests() -> dict:
     req['responses']['auto'].headers = {
         'Content-Type': 'text/html',
     }
+    req['responses']['auto'].history = []
+    req['responses']['auto'].request = UserDict()
+    req['responses']['auto'].request.headers = UserDict()
     req['responses']['auto'].status_code = 200
-    req['responses']['auto'].url = 'https://http-observatory.services.mozilla.com'
+    req['responses']['auto'].url = 'https://http-observatory.services.mozilla.com/'
 
     req['session'].cookies = CookieJar()
 
+    req['responses']['cors'] = deepcopy(req['responses']['auto'])
     req['responses']['http'] = deepcopy(req['responses']['auto'])
     req['responses']['https'] = deepcopy(req['responses']['auto'])
 
