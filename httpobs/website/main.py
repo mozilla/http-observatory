4c80e912ed5a3b4cdcee20571ef1393bf40a16e4
April King <april@mozilla.com>
2016-03-11 11:51:14 -0600

    Update things to work with uwsgi

--- httpobs/website/main.py
+++ httpobs/website/main.py
@@ -5,7 +5,28 @@ from os import environ
 
 from httpobs.website import add_response_headers
 
+
+def __exit_with(msg: str) -> None:
+    print(msg)
+    sys.exit(1)
+
+# Register the application with flask
 app = Flask('http-observatory')
+if environ.get('HTTPOBS_ENVIRONMENT') == 'backend':
+    print('Loading the HTTP Observatory Backend')
+    from httpobs.website.backend import api
+elif environ.get('HTTPOBS_ENVIRONMENT') == 'frontend':
+    print('Loading the HTTP Observatory Frontend')
+    from httpobs.website.frontend import api
+else:
+    __exit_with('HTTPOBS_ENVIRONMENT not set. Exiting.')
+app.register_blueprint(api)
+
+# Check to make sure we have all the needed environmental variables set
+if 'HTTPOBS_API_KEY' not in environ:
+    __exit_with('HTTPOBS_API_KEY not set. Exiting.')
+elif environ.get('HTTPOBS_ENVIRONMENT') == 'frontend' and 'HTTPOBS_BACKEND_URL' not in environ:
+    __exit_with('HTTPOBS_BACKEND_URL not set. Exiting.')
 
 
 @app.route('/')
@@ -14,28 +35,10 @@ def main() -> str:
     return 'Welcome to the HTTP Observatory!'
 
 
-def __exit_with(msg: str) -> None:
-    print(msg)
-    sys.exit(1)
-
 if __name__ == '__main__':
-    if environ.get('HTTPOBS_ENVIRONMENT') == 'backend':
-        from httpobs.website.backend import api
-        port = 57002
-    elif environ.get('HTTPOBS_ENVIRONMENT') == 'frontend':
-        from httpobs.website.frontend import api
-        port = 57001
-    else:
-        __exit_with('HTTPOBS_ENVIRONMENT not set. Exiting.')
-
-    # Check to make sure we have all the needed environmental variables set
-    if 'HTTPOBS_API_KEY' not in environ:
-        __exit_with('HTTPOBS_API_KEY not set. Exiting.')
-    elif environ.get('HTTPOBS_ENVIRONMENT') == 'frontend' and 'HTTPOBS_BACKEND_URL' not in environ:
-        __exit_with('HTTPOBS_BACKEND_URL not set. Exiting.')
+    port = 57001 if environ['HTTPOBS_ENVIRONMENT'] == 'frontend' else 57002
 
     # Enable debugging, if HTTPOBS_DEV is set
     debug = True if 'HTTPOBS_DEV' in environ else False
 
-    app.register_blueprint(api)
     app.run(debug=debug, port=port)
