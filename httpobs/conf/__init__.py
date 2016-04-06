75836fcf3403af5299fa43dc8be9b710ec2e4022
April King <april@mozilla.com>
2016-04-06 16:56:19 -0500

    Various updates to the backend scanner and separation into httpobs.conf

--- httpobs/conf/__init__.py
+++ httpobs/conf/__init__.py
@@ -13,6 +13,7 @@ def __exit_without(envvar: str) -> str:
 
     return value
 
+# TODO: make this whole thing a configuration file
 
 DEVELOPMENT_MODE = True if environ.get('HTTPOBS_DEV', False) == 'true' else False
 
@@ -22,14 +23,16 @@ COOLDOWN = 15 if DEVELOPMENT_MODE else 1200
 API_KEY = environ.get('HTTPOBS_API_KEY')
 API_URL = environ.get('HTTPOBS_API_URL',
                       'https://http.observatory.services.mozilla.com/api/v1')
+
+# Broker configuration
 BROKER_URL = __exit_without('HTTPOBS_BROKER_URL')
+
+# Database configuration
 DATABASE_DB = environ.get('HTTPOBS_DATABASE_DB', 'http_observatory')
 DATABASE_HOST = environ.get('HTTPOBS_DATABASE_HOST', 'localhost')
 DATABASE_PASSWORD = __exit_without('HTTPOBS_DATABASE_PASS')
 DATABASE_PORT = environ.get('HTTPOBS_DATABASE_PORT', 5432)
 DATABASE_USER = __exit_without('HTTPOBS_DATABASE_USER')
-WEBSITE_PORT = environ.get('HTTPOBS_WEBSITE_PORT', 57001)
-
 
 # Set some database provider specific parameters
 __dirname = os.path.abspath(os.path.dirname(__file__))
@@ -40,9 +43,17 @@ else:
     DATABASE_CA_CERT = None
     DATABASE_SSL_MODE = 'prefer'
 
+# Retriever parameters
+RETRIEVER_CONNECT_TIMEOUT = 6.05
+RETRIEVER_READ_TIMEOUT = 30
+RETRIEVER_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:45.0) Gecko/20100101 Firefox/45.0'
+
 # The scanner should back off once the system load average reaches a specific load factor
-SCANNER_MAX_LOAD_RATIO = 3
-SCANNER_MAX_LOAD = cpu_count() * SCANNER_MAX_LOAD_RATIO
 SCANNER_BROKER_RECONNECTION_SLEEP_TIME = 15
 SCANNER_CYCLE_SLEEP_TIME = .5  # half a second
 SCANNER_DATABASE_RECONNECTION_SLEEP_TIME = 5
+SCANNER_MAX_LOAD_RATIO = 3
+SCANNER_MAX_LOAD = cpu_count() * SCANNER_MAX_LOAD_RATIO
+SCANNER_MOZILLA_DOMAINS = ('mozilla', 'allizom', 'browserid', 'firefox', 'persona', 'taskcluster', 'webmaker')
+
+WEBSITE_PORT = environ.get('HTTPOBS_WEBSITE_PORT', 57001)
