c511c53ddb94c4bf6a8af3b7c8bd2a38c766fe0e
April King <april@mozilla.com>
2016-11-04 09:55:12 -0500

    move the standalone CLI to a separate file

--- httpobs/scanner/local.py
+++ httpobs/scanner/local.py
@@ -1,12 +1,8 @@
-import argparse
 import httpobs.conf
-import json
 
 from httpobs.scanner.analyzer import NUM_TESTS, tests
 from httpobs.scanner.grader import get_grade_and_likelihood_for_score, get_score_description
 from httpobs.scanner.retriever import retrieve_all
-
-from operator import itemgetter
 
 
 def scan(hostname, **kwargs):
@@ -78,84 +74,3 @@ def scan(hostname, **kwargs):
         },
         'tests': {result.pop('name'): result for result in results}
     })
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-
-    # Add the various arguments
-    parser.add_argument('--http-port',
-                        default=80,
-                        help='port to use for the HTTP scan (instead of 80)',
-                        type=int)
-    parser.add_argument('--https-port',
-                        default=443,
-                        help='port to use for the HTTPS scan (instead of 443)',
-                        type=int)
-    parser.add_argument('--path',
-                        default=argparse.SUPPRESS,
-                        help='path to scan, instead of /',
-                        type=str)
-    parser.add_argument('--no-verify',
-                        action='store_true',
-                        help='disable certificate verification in the HSTS/HPKP tests')
-    parser.add_argument('--cookies',
-                        default=argparse.SUPPRESS,
-                        help='cookies to send in scan (json formatted)',
-                        type=json.loads)
-    parser.add_argument('--headers',
-                        default=argparse.SUPPRESS,
-                        help='headers to send in scan (json formatted)',
-                        type=str)
-    parser.add_argument('--format',
-                        default='json',
-                        help='output format (json or report), default of json',
-                        type=str)
-    parser.add_argument('hostname',
-                        help='host to scan',
-                        type=str)
-
-    args = vars(parser.parse_args())
-
-    # Remove the -- from the name, change - to underscore
-    args = {k.split('--')[-1].replace('-', '_'): v for k, v in args.items()}
-    format = args.pop('format').lower()
-
-    # print out help if no arguments are specified, or bad arguments
-    if len(args) == 0 or format not in ('json', 'report'):
-        parser.print_help()
-        parser.exit(-1)
-
-    # Because it makes sense this way
-    if args['http_port'] == 80:
-        del(args['http_port'])
-
-    if args['https_port'] == 443:
-        del (args['https_port'])
-
-    if args.pop('no_verify'):
-        args['verify'] = False
-
-    # Get the scan results
-    r = scan(**args)
-
-    # print out the results to the command line
-    if format == 'json':
-        print(json.dumps(r, indent=4, sort_keys=True))
-    elif format == 'report':
-        print('Score: {0} [{1}]'.format(r['scan']['score'],
-                                        r['scan']['grade']))
-
-        print('Modifiers:')
-
-        # Get all the scores by test name
-        scores = [[k.replace('-', ' ').title(), v['score_modifier'], v['score_description']]
-                  for k, v in r['tests'].items()]
-        scores = sorted(scores, key=itemgetter(0))  # [('test1', -5, 'foo'), ('test2', -10, 'bar')]
-
-        for score in scores:
-            if score[1] > 0:
-                score[1] = '+' + str(score[1])  # display 5 as +5
-            print('  {test:<30} [{modifier:>3}]  {reason}'.format(test=score[0],
-                                                                  modifier=score[1],
-                                                                  reason=score[2]))
