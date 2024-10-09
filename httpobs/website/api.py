382a2948a1433e4b415414c1412bc5cbbcb31012
Andi Pieper <apieper@mozilla.com>
2024-10-09 12:23:33 +0200

    linter

--- httpobs/website/api.py
+++ httpobs/website/api.py
@@ -10,11 +10,7 @@ from httpobs import STATE_FAILED, STATE_RUNNING, STATES
 from httpobs.conf import API_ALLOW_VERBOSE_STATS_FROM_PUBLIC, API_COOLDOWN, DEVELOPMENT_MODE
 from httpobs.scanner import scan
 from httpobs.scanner.grader import GRADES, get_score_description
-from httpobs.website import (
-    add_response_headers,
-    add_sunset_headers,
-    sanitized_api_response,
-)
+from httpobs.website import add_response_headers, add_sunset_headers, sanitized_api_response
 from httpobs.website.utils import valid_hostname
 
 api = Blueprint('api', __name__)
